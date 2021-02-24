cd3b7eb401eb82126bb8b78934cd00087a1d78ec
Vinicius Emanoel Andrade <viniciusemanaoelandrade@hotmail.com>
2021-02-24 20:42:21 -0300

    Estrutura base do projeto.

---
+++ app/src/main/java/company/entree/android_architecture_course/MainActivity.kt
@@ -0,0 +1,11 @@
+package company.entree.android_architecture_course
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
