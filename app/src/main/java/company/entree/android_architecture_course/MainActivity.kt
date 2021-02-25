7667c66dca5cc8e312b0b0fe71f443f237f0e7ea
Vinicius Emanoel Andrade <viniciusemanaoelandrade@hotmail.com>
2021-02-24 22:48:49 -0300

    Exemplo inserindo um objeto na view utilizando o dataBinding.

--- app/src/main/java/company/entree/android_architecture_course/MainActivity.kt
+++ app/src/main/java/company/entree/android_architecture_course/MainActivity.kt
@@ -2,10 +2,19 @@ package company.entree.android_architecture_course
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import company.entree.android_architecture_course.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.student = this.getStudent()
+    }
+
+    private fun getStudent(): Student {
+        return Student(1, "Vinicius", "vinicius@gmail.com")
     }
 }
---
+++ app/src/main/java/company/entree/android_architecture_course/Student.kt
@@ -0,0 +1,7 @@
+package company.entree.android_architecture_course
+
+data class Student(
+    var id: Int,
+    var name: String,
+    var email: String
+)
