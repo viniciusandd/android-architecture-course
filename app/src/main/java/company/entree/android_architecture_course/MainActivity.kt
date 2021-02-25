84db641003cdb8e94dcc2c11fdb5dc3d02963a96
Vinicius Emanoel Andrade <viniciusemanaoelandrade@hotmail.com>
2021-02-24 21:23:02 -0300

    Exemplo básico de como utilizar o dataBinding.

--- app/src/main/java/company/entree/android_architecture_course/MainActivity.kt
+++ app/src/main/java/company/entree/android_architecture_course/MainActivity.kt
@@ -2,10 +2,23 @@ package company.entree.android_architecture_course
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import company.entree.android_architecture_course.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.btnSend.setOnClickListener {
+            this.displayGreeting()
+        }
+    }
+
+    private fun displayGreeting() {
+        binding.apply {
+            txtGreetings.text = "Hello ${inputPersonName.text}"
+        }
     }
 }
