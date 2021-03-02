1140cb260385ae77c139a242f1414bcb7c4fae73
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2021-03-02 20:39:52 -0300

    Inicializando o DataBinding.

--- app/src/main/java/company/entree/android_architecture_course/MainActivity.kt
+++ app/src/main/java/company/entree/android_architecture_course/MainActivity.kt
@@ -2,10 +2,13 @@ package company.entree.android_architecture_course
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import company.entree.android_architecture_course.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
